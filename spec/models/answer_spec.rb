38458ba0777d71fd07f9b4f7553a7ed2b85e06c3
Annie Coleman <annierm18@yahoo.com>
2018-05-01 19:42:06 -0700

    assignment 14: pt 2

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: [false]) }
-  let(:answer) { Answer.create!(body: 'Comment Body', answer: answer) }
+  let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
 
   describe "attributes" do
     it "has a body attribute" do
