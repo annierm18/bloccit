a41e4fc5fa9ee6b9a1845f4ad0d5714c87e6818b
Annie Coleman <annierm18@yahoo.com>
2018-05-08 18:50:49 -0700

    assignment 18: completed

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -59,18 +59,18 @@ RSpec.describe AdvertisementsController, type: :controller do
     end
   end
 
-  describe "ADVERTISEMENT create" do
+  describe "POST create" do
     it "increases the number of advertisements by 1" do
-      expect{ advertisement :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } } }.to change(Advertisement,:count).by(1)
+      expect{ post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } } }.to change(Advertisement,:count).by(1)
     end
 
     it "assigns the new advertisement to @advertisement" do
-      advertisement :create, params: { advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } }
+      post :create, params: { advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } }
       expect(assigns(:advertisement)).to eq Advertisement.last
     end
 
     it "redirects to the new advertisment" do
-      advertisement :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } }
+      post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } }
       expect(response).to redirect_to Advertisement.last
     end
   end
