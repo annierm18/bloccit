e7083c34d12aaefbc6d81295678ade2c74f20dee
Annie Coleman <annierm18@yahoo.com>
2018-05-08 22:01:04 -0700

    assignment 19: pt1

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,23 @@ posts = Post.all
   )
 end
 
+50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph
+    price: RandomData.random_paragraph
+  )
+end
+
+
+100.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+    resolved: false
+  )
+end
+
 puts "#{Post.count}"
 post = Post.find_or_create_by(title: "A unique title", body: "A unique body")
 puts "#{Post.count}"
@@ -27,22 +44,5 @@ puts "#{Post.count}"
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-
-
-50.times do
-  Advertisement.create!(
-    title: RandomData.random_sentence,
-    copy: RandomData.random_paragraph
-    price: RandomData.random_paragraph
-  )
-end
-advertisements = Advertisement.all
-
-
-puts "#{Advertisement.count}"
-post = Advertisement.find_or_create_by(title: "A unique title", copy: [text], price: [integer])
-puts "#{Advertisement.count}"
-
-
-puts "Seed finished"
 puts "#{Advertisement.count} advertisements created"
+puts "#{Question.count} questions created"
