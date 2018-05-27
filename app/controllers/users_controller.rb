8a32cc735bee442fd787625eac26523e432e8223
Annie Coleman <annierm18@yahoo.com>
2018-05-26 20:07:20 -0700

    assignment 24: pt1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+  end
+
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
@@ -18,4 +27,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def user_params
+   params.require(:user).permit(:email, :password, :password_confirmation)
+ end
+
+ helper_method :user_params
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   resources :questions
   resources :advertisements
 
+  post 'users/confirm' =>'users#confirm'
+
   get 'about' => 'welcome#about'
 
   get 'welcome/contact'
