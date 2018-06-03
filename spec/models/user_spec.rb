8c0c5f741e770e76014e489af68b5e3e7a45c367
Annie Coleman <annierm18@yahoo.com>
2018-06-02 20:26:22 -0700

    assignment 27: pt2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,10 @@ it { is_expected.to have_many(:posts) }
       expect(user).to respond_to(:admin?)
     end
 
+    it "responds to moderator?" do
+      expect(user).to respond_to(:moderator?)
+    end
+
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
@@ -46,6 +50,30 @@ it { is_expected.to have_many(:posts) }
         it "returns false for #admin?" do
           expect(user.admin?).to be_falsey
         end
+
+        it "returns false for #moderator?" do
+          expect(user.moderator?).to be_falsey
+        end
+      end
+
+      context "moderator user" do
+        before do
+          user.moderator!
+        end
+        
+        it "returns false for #member?" do
+          expect(user.member?).to be_falsey
+        end
+
+        it "returns true for #moderator?" do
+          expect(user.moderator?).to be_truthy
+        end
+
+        it "returns false for #admin?" do
+          expect(user.admin?).to be_falsey
+        end
+
+
       end
 
       context "admin user" do
@@ -55,6 +83,10 @@ it { is_expected.to have_many(:posts) }
 
         it "returns false for #member?" do
           expect(user.member?).to be_falsey
+        end
+
+        it "returns false for #moderator?" do
+          expect(user.moderator?).to be_falsey
         end
 
         it "returns true for #admin?" do
